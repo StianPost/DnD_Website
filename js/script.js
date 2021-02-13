e4a076a89070e9ac2c6a9715bf48c2f473db8323
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-13 23:35:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const mainAPI = 'http://www.dnd5eapi.co/api';
 // filler
 // DO WORK YOU DUMB DUMB
+
+async function getDnd(url) {
+  response = await fetch(url);
+  console.log(response);
+  result = await response.json();
+  console.log(result);
+  console.log(result.classes);
+}
+
+getDnd(mainAPI);
